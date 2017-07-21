c07ce23524e93dccbc0b2d8eb6a3071499331c2e
Arif Liminto <arifliminto86@hotmail.com>
2017-07-21 16:03:38 +0800

    add ramen

---
+++ RamenGo/Script/Ramen.js
@@ -0,0 +1,29 @@
+﻿
+/**
+ * Class for calculate how many plates that they eat
+ */
+function Ramen()
+{
+    var FULLPLATESCOUNT = 5;
+    var _count = 0;
+    var _plates = 0;
+
+    this.Eat = function (divid)
+    {
+        _count += 1;        
+        if (_count >= FULLPLATESCOUNT)
+        {
+            _plates += 1;
+            this.ShowScore(divid);            
+            _count = 0;
+        }
+    };
+
+    this.ShowScore = function (divid)
+    {
+        var str = "<span class='label label-default>" + _plates + "</span>";              
+        $("#" + divid).html("<b> Plates: "+_plates+"</b>");
+        //$("#divPlates").append(str);
+        
+    }
+}
